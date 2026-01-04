0b0685e88750d33e26331866d06bb850342f6692
zefan <lordzefan@gmail.com>
2026-01-04 14:31:37 +0700

    Menambah animasi hit dan memberikan partikal blood ketika terkena hit

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -7,11 +7,18 @@ public class PlayerHealth : MonoBehaviour
  public int curentHealth, maxHealth;
  Animator animator;
  CharacterController characterController;
+ public AudioClip swordHitSfx;
+ public AudioClip []playerHitSfx;
+ public AudioClip deadSfx;
+ AudioSource audioSource;
+ public GameObject bloodVfx;
+ public Transform bloodPoint;
 
  public bool isDead;
 
     void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
     }
@@ -23,12 +30,20 @@ public class PlayerHealth : MonoBehaviour
 
  public void TakeDamage(int damage)
  {
+    Instantiate(bloodVfx, bloodPoint.position, Quaternion.identity);
+    audioSource.PlayOneShot(swordHitSfx);
+    animator.Play("Hit");
     curentHealth -= damage;
     if(isDead)return;
     if (curentHealth <=0)
     {
+        audioSource.PlayOneShot(deadSfx);
         OnDead();
     }
+        else
+        {
+           audioSource.PlayOneShot(playerHitSfx[Random.Range(0, playerHitSfx.Length)]) ;
+        }
  }
 
  public void OnDead()
